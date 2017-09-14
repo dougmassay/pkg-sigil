3fa108177d575338007b6796ed8610ba1cc3c982
Doug Massay <dmassaygithub@gmail.com>
2017-09-14 17:47:24 -0400

    Update libxml2 and libxslt (no need to patch libxml2-2.9.5)

--- scripts/pkgs/libxml2.py
+++ scripts/pkgs/libxml2.py
@@ -14,7 +14,7 @@ from .utils import ModifiedEnv, apply_patch, simple_build, replace_in_file
 
 def main(args):
     with ModifiedEnv(LD_LIBRARY_PATH=LIBDIR):
-        apply_patch('libxml2_bug_fix.patch')
+        # apply_patch('libxml2_bug_fix.patch')
         simple_build('--disable-dependency-tracking --disable-static --enable-shared --without-python --without-debug --with-iconv={0} --with-zlib={0}'.format(
             PREFIX), configure_name='./autogen.sh')
         replace_in_file(os.path.join(build_dir(), 'lib/pkgconfig/libxml-2.0.pc'), re.compile(br'^prefix=.+$', re.M), b'prefix=%s' % PREFIX)
